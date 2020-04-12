1b8d37b36e83eda790851e7e4d35425662c0f679
joeyzak1 <joeyzmassey@gmail.com>
2020-04-12 14:04:25 -0400

    Added graphics template

--- main.py
+++ main.py
@@ -3,4 +3,40 @@ main.py
 ICS3U FSE
 
 Run this file to play the game
+
 '''
+from pygame import *
+
+init()
+width, height = 800, 600
+screen = display.set_mode((width,height))
+
+RED = (255,0,0)
+GREY = (127,127,127)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
+YELLOW = (255,255,0)
+
+running = True
+myClock = time.Clock()
+
+
+
+
+
+while running:
+    for evt in event.get():
+        if evt.type == QUIT:
+            running=False
+                       
+    mx, my = mouse.get_pos()
+    mb = mouse.get_pressed()
+    keys = key.get_pressed()
+      
+   
+    myClock.tick(60) #60 fps
+    display.flip()
+            
+quit()
